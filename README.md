654615c0f3e1341ea6804c4e4249ae23f8d40fc7
Naor_Shlomo <naorsh19@gmail.com>
2022-04-04 20:50:27 +0300

    Dev env is ready

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tech-project
+docker-compose up --build -d && docker exec -it tech-project-naor-and-oren-image-1 /bin/bash
---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Naor_Shlomo on 04/04/2022.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
